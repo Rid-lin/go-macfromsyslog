86b843d7fe26a94ad7070471f949f0670da81aee
Rid-lin <vlad@vegner.org>
2020-12-04 12:29:44 +0500

    Code refactoring. Moved graceful shutdown into a separate function. Added marshaling to JSON in response. Changed the answer: vmseto just "mac-address" now gives the structure, for future extensions. Fixed: incorrect parsing of the log level. Removed: using gorilla / mux package. Moved notifications in debug-mode of logs to trace-mode.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
@@ -12,17 +13,16 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gorilla/mux"
 	// "github.com/gorilla/sessions"
 	"github.com/hpcloud/tail"
 	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
 type Config struct {
 	LogLevel           string `yaml:"LogLevel" toml:"loglevel" env:"LOG_LEVEL"`
 	NameSyslogFileName string `yaml:"SyslogFile" toml:"syslog" env:"SYSLOG_FILE"`
+	BindAddr           string `yaml:"BindAddr" toml:"bindaddr" env:"ADDR_M4S" default:":3030"`
 }
 type request struct {
 	Time,
@@ -30,6 +30,9 @@ type request struct {
 	timeInt int
 }
 
+type response struct {
+	Mac string `JSON:"Mac"`
+}
 type lineInMap struct {
 	month,
 	day,
@@ -59,7 +62,7 @@ type transport struct {
 var (
 	cfg Config
 	// writer         *bufio.Writer
-	NameSyslogFile *os.File
+	SyslogFile *os.File
 	// err            error
 	configFilename string = "config.toml" //need change
 )
@@ -67,6 +70,7 @@ var (
 func init() {
 	flag.StringVar(&cfg.LogLevel, "loglevel", "info", "Log level")
 	flag.StringVar(&cfg.NameSyslogFileName, "syslog", "syslog.log", "The file where logs will be written in the format of squid logs")
+	flag.StringVar(&cfg.BindAddr, "addr", ":3030", "Listen address")
 	flag.Parse()
 	var config_source string
 	if cfg.NameSyslogFileName == "" {
@@ -74,16 +78,17 @@ func init() {
 		if err != nil {
 			log.Warningf("No config file(%v) found: %v", configFilename, err)
 		}
-		lvl, err2 := log.ParseLevel(cfg.LogLevel)
-		if err2 != nil {
-			log.Errorf("Error in determining the level of logs (%v). Installed by default = Info", cfg.LogLevel)
-			lvl, _ = log.ParseLevel("info")
-		}
-		log.SetLevel(lvl)
 		config_source = "ENV/CFG"
 	} else {
 		config_source = "CLI"
 	}
+
+	lvl, err := log.ParseLevel(cfg.LogLevel)
+	if err != nil {
+		log.Errorf("Error in determining the level of logs (%v). Installed by default = Info", cfg.LogLevel)
+		lvl, _ = log.ParseLevel("info")
+	}
+	log.SetLevel(lvl)
 
 	log.Debugf("Config read from %s: NameSyslogFile=(%v)",
 		config_source,
@@ -166,7 +171,7 @@ func (data *transport) parseLineLog(lineIn string) (lineOfLog, error) {
 	lineInMap.ip = lineInSlice[7]     // 192.168.65.149
 	lineInMap.direct = lineInSlice[8] // from or to
 	lineInMap.mac = lineInSlice[9]    // 04:D3:B5:FC:E8:09
-	log.Debugf("month:'%v',day:'%v',time:'%v',parent:'%v',info:'%v',iface:'%v',method:'%v',ip:'%v',direct:'%v',mac:'%v'",
+	log.Tracef("month:'%v',day:'%v',time:'%v',parent:'%v',info:'%v',iface:'%v',method:'%v',ip:'%v',direct:'%v',mac:'%v'",
 		lineInMap.month,
 		lineInMap.day,
 		lineInMap.time,
@@ -223,7 +228,7 @@ func (data *transport) getDataFromSyslog(t *tail.Tail) {
 		for line := range t.Lines {
 			lineOfLog, err := data.parseLineLog(line.Text)
 			if err != nil {
-				log.Debugf("%v", err)
+				log.Tracef("%v", err)
 				continue
 			}
 
@@ -238,32 +243,74 @@ func (data *transport) getDataFromSyslog(t *tail.Tail) {
 	}
 }
 
-type server struct {
-	router *mux.Router
-	logger *logrus.Logger
-	store  *transport
-	// sessionStore sessions.Store
-}
-
-func newServer(store transport /*sessionStore sessions.Store*/) *server {
-	s := &server{
-		router: mux.NewRouter(),
-		logger: logrus.New(),
-		store:  &store,
-		// sessionStore: sessionStore,
+func handleIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w,
+			`<html>
+			<head>
+			<title>go-macfromsyslog</title>
+			</head>
+			<body>
+			Более подробно на https://github.com/Rid-lin/go-macfromsyslog
+			</body>
+			</html>
+			`)
 	}
-
-	s.configureRouter()
-
-	return s
 }
 
-func (s *server) configureRouter() {
-	s.router.HandleFunc("/", s.handleIndex()).Methods("GET")
-	s.router.HandleFunc("/getmac", s.getmac()).Methods("GET")
+func (data *transport) getmac() http.HandlerFunc {
+	var (
+		request  request
+		Response response
+	)
 
-	// для отдачи сервером статичных файлов из папки web
-	s.router.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web"))))
+	return func(w http.ResponseWriter, r *http.Request) {
+		request.Time = r.URL.Query().Get("time")
+		request.IP = r.URL.Query().Get("ip")
+		Response.Mac = data.GetMac(&request)
+		log.Debugf(" | Request:'%v','%v' response:'%v'", request.Time, request.IP, Response.Mac)
+		responseJSON, err := json.Marshal(Response)
+		if err != nil {
+			log.Errorf("Error Marshaling mac'%v'to JSON:'%v'", Response.Mac, err)
+		}
+		// fmt.Fprint(w, mac)
+		_, err2 := w.Write(responseJSON)
+		if err2 != nil {
+			log.Errorf("Error send response:%v", err2)
+		}
+	}
+}
+
+type shutdownType struct {
+	SyslogFile *os.File
+	t          *tail.Tail
+	exitChan   chan os.Signal
+}
+
+func (stop *shutdownType) grecefullShutdown() {
+	<-stop.exitChan
+	// HERE Insert commands to be executed before the program terminates
+	// writer.Flush()
+	log.Infoln("Attempt to shutdown")
+	stop.t.Cleanup()
+	log.Infoln("Removes inotify watches ")
+	if err := stop.t.Stop(); err != nil {
+		log.Error("Error stops the tailing activity", err)
+	}
+	log.Infoln("Stops the tailing activity")
+	stop.SyslogFile.Close()
+	log.Infoln("Close the open file")
+	log.Infoln("Shutting down")
+	os.Exit(0)
+
+}
+
+func newGrecefullShutdown(t *tail.Tail, file *os.File, exitChan chan os.Signal) *shutdownType {
+	return &shutdownType{
+		t:          t,
+		SyslogFile: file,
+		exitChan:   exitChan,
+	}
 }
 
 func main() {
@@ -280,27 +327,40 @@ func main() {
 	data := NewTransport()
 	go data.getDataFromSyslog(t)
 
-	go func() {
-		<-exitChan
-		// HERE Insert commands to be executed before the program terminates
-		// writer.Flush()
-		log.Debugln("Attempt to shutdown")
-		t.Cleanup()
-		log.Debugln("Removes inotify watches ")
-		t.Stop()
-		log.Debugln("Stops the tailing activity")
-		NameSyslogFile.Close()
-		log.Debugln("Close the open file")
-		log.Println("Shutting down")
-		os.Exit(0)
+	http.HandleFunc("/", handleIndex())
+	http.HandleFunc("/getmac", data.getmac())
 
+	go func() {
+		err := http.ListenAndServe(cfg.BindAddr, nil)
+		if err != nil {
+			log.Fatalf("HTTP-Server return error:%v", err)
+		}
 	}()
+
+	// go func() {
+	// 	<-exitChan
+	// 	// HERE Insert commands to be executed before the program terminates
+	// 	// writer.Flush()
+	// 	log.Infoln("Attempt to shutdown")
+	// 	t.Cleanup()
+	// 	log.Infoln("Removes inotify watches ")
+	// 	t.Stop()
+	// 	log.Infoln("Stops the tailing activity")
+	// 	SyslogFile.Close()
+	// 	log.Infoln("Close the open file")
+	// 	log.Infoln("Shutting down")
+	// 	os.Exit(0)
+
+	// }()
+	stop := newGrecefullShutdown(t, SyslogFile, exitChan)
+
+	go stop.grecefullShutdown()
 
 	for {
 		fmt.Scan(&request.Time, &request.IP)
 		s := data.GetMac(&request)
 		fmt.Println(s)
-		log.Debugf(" | Request:'%v','%v' response:'%v'", request.Time, &request.IP, s)
+		log.Debugf(" | Request:'%v','%v' response:'%v'", request.Time, request.IP, s)
 	}
 
 }
